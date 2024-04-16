5c3b05ab24d8dacebe0dc4d593ea6cda231fe02f
kostas-rigan <t8200145@aueb.gr>
2024-04-16 23:02:48 +0300

    update powershell script

--- run.ps1
+++ run.ps1
@@ -1,4 +1,7 @@
+$currentPath = (Get-Item .).FullName
+New-Item "temp" -ItemType Directory > $null
 For($i=0;$i -le 10; $i++) {
     Write-Host "Hello world"
 }
 python mapper.py | python reducer.py
+Remove-Item -path $currentPath\temp -Recurse
